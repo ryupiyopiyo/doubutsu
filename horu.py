fdc7c156785c640fbc5667fb2b1012e84dc73caa
gac <postmaster@gaccag.com>
2021-07-27 23:45:03 +0900

    horu.py

---
+++ horu.py
@@ -0,0 +1,17 @@
+import logging
+from JoycontrolPlugin import JoycontrolPlugin
+
+logger = logging.getLogger(__name__)
+
+
+class RepeatA(JoycontrolPlugin):
+    async def run(self):
+        logger.info('================HORU==================')
+
+        while True:
+
+            await self.left_stick('up')
+            await self.wait(1.0)
+
+            await self.button_push('a')
+            await self.wait(1.0)
